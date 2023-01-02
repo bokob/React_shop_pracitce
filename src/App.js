776caaf58060d8d64e4df29ffb2976fdebb374ec
bokob <bmc0520@naver.com>
2023-01-02 22:50:59 +0900

    반복되는 컴포넌트 숙제 클리어, 상세페이지 만들기 숙제 클리어

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import {Container, Nav, Navbar} from 'react-bootstrap';
-import './App.css';
-import {useState} from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import './App.css'
+import { useState } from 'react'
 import data from './data.js'
-import Col from './component.js';
+import Col from './component.js'
+import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
-
-  let [shoes] = useState(data);
+  let [shoes] = useState(data)
 
   return (
     <div className="App">
@@ -19,25 +19,60 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
-      
-      <div className="main-bg"></div>
-    
-      <div>
-        <br></br>
-      <div class="container text-center">
-          <div class="row">
-            {
-            shoes.map(function(item,i){
-              return(
-                <Col url = {shoes[i].jpg} title = {shoes[i].title} price = {shoes[i].price}/>
-              )
-            })
-            }
-          </div>
+
+      <Link to="/">홈</Link>
+      <Link to="/detail">상세페이지</Link>
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="main-bg"></div>
+              <div>
+                <br></br>
+                <div class="container text-center">
+                  <div class="row">
+                    {shoes.map(function (item, i) {
+                      return (
+                        <Col
+                          url={shoes[i].jpg}
+                          title={shoes[i].title}
+                          price={shoes[i].price}
+                        />
+                      )
+                    })}
+                  </div>
+                </div>
+              </div>
+            </>
+          }
+        />
+        <Route path="/detail" element={<Detail />} />
+      </Routes>
+    </div>
+  )
+}
+
+function Detail() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <img
+            src="https://codingapple1.github.io/shop/shoes1.jpg"
+            width="100%"
+          />
+        </div>
+        <div className="col-md-6">
+          <h4 className="pt-5">상품명</h4>
+          <p>상품설명</p>
+          <p>120000원</p>
+          <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
