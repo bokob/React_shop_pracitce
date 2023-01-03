e7bf0faad446ddce32fa03a81dcf9db3ecf599bf
bokob <bmc0520@naver.com>
2023-01-03 17:06:53 +0900

    2초 있다 사라지는 숙제 완료

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,11 +1,22 @@
 import { useParams } from "react-router-dom";
 import data from '../data.js';
 import styled from "styled-components";
-import React, { useEffect, useState, useRef} from "react";
+import React, { useEffect, useState} from "react";
 
 function Detail(props) {
 
+  let [warning, setWarning] = useState(1)
+  let [test, setTest] = useState(0)
+
+  let flag=0;
   
+  useEffect(()=>{
+      setTimeout(()=>{
+        setWarning(0)},2000);
+  
+      // setTimeout(()=>{
+      //   setWarning(1)},1000);
+    });
 
     let {id} = useParams();
 
@@ -17,9 +28,9 @@ function Detail(props) {
 
     return (
       <div className="container">
-        <div className="alert alert-warning">
-          2초 이내 구매시 할인
-        </div>
+        
+        <Warning warning={warning} />
+
         <div className="row">
           <div className="col-md-6">
             <img src={'https://codingapple1.github.io/shop/shoes'+ (find_item.id + 1) +'.jpg'}
@@ -34,6 +45,19 @@ function Detail(props) {
         </div>
       </div>
     )
+  }
+
+  function Warning(props){
+
+    let result="";
+    if(props.warning==1)
+      result= <div className="alert alert-warning">
+      2초 이내 구매시 할인
+      </div>;
+    else
+      result="";
+
+    return(result);
   }
 
   export default Detail;
